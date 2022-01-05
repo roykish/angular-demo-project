7233444686f0ed6531a24ef13b58ca58eb7afd73
Kishor Roy <roykishor93@gmail.com>
2022-01-05 11:16:35 +0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { Employee } from './employee';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+const routes: Routes = [
+  {path: 'employees', component: EmployeeListComponent},
+  {path: 'cretae-employee', component:CreateEmployeeComponent},
+  {path: '', redirectTo:'employees', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
